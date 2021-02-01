dc1472fd8239cf8764211269b1258ff8306f9b1d
0TTT <67382045+0TTT@users.noreply.github.com>
2021-02-01 12:03:51 +0800

    Add files via upload

--- src/ui/platforms/desktop/src/pages/lobby/ui/feedback_dialog/feedback_content.ts
+++ src/ui/platforms/desktop/src/pages/lobby/ui/feedback_dialog/feedback_content.ts
@@ -3,5 +3,7 @@ export const feedBackContent = `
 <b>新神杀交流1群：196873175（已满）</b>
 <b>新神杀交流2群：942355199 </b>
 <br />
-<h4>如提示需更新客户端可前往<a href="https://pan.baidu.com/s/13l_dYH2MtcUKlIgwoTOi5w" target="_blank">百度盘（提取码：n57e）</a>下载最新版本</h4>
+<h4>如提示需更新客户端可前往<a href="https://pan.baidu.com/s/13l_dYH2MtcUKlIgwoTOi5w" target="_blank">百度盘（提取码：n57e）</a>
+<a href="https://cloud.189.cn/t/MJNBNjfqyu6b " target="_blank">天翼云(访问码:1yfh)</a>下载最新版本</h4>
+
 `;
